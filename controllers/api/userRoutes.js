e6cbb44a63ed0c0d486f61feaab41a11d9f01e59
bdurham227 <bdurham227@gmail.com>
2021-05-08 13:28:12 -0400

    required handlebars and .create({}) new isntance environment of handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,50 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+router.get('/', async (req, res) => {
+  try {
+
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+
+      order: ["name", "ASC" ],
+    });
+    const users = userData.map((user) => {
+      user.get({ plain: true });
+    })
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+// router.get('/login', async (req, res) => {
+//   try {
+//     if (req.session.logged_in) {
+//       res.redirect('/');
+//       return;
+//     }
+
+//     res.render('/login');
+
+
+
+//   } catch (err) {
+//     res.status(500).json(err)
+//   }
+// })
+
+
+
+
+
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/auth');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -22,8 +25,18 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
+
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
